0393860ef769ff89c90b29d3faaa98a5f0a07b59
Ines <manoussa@users.noreply.github.com>
2017-05-09 20:37:07 +0200

    new tests

--- labyrinth/test/labyrinth/test.clj
+++ labyrinth/test/labyrinth/test.clj
@@ -23,3 +23,48 @@
                    [1 :W :W :W]
                    [1 1 1 1]]]
     (is (= solution (solve labyrinth))))))
+
+(deftest test-starting-point
+  (testing
+   (let [labyrinth [[:W :W 1 :W]
+                    [:W :W :_ :W]
+                    [:W :W :_ :W]
+                    [:W :W :_ :Exit]]
+         solution [[:W :W 1 :W]
+                   [:W :W 1 :W]
+                   [:W :W 1 :W]
+                   [:W :W 1 1]]]
+    (is (= solution (solve labyrinth))))))
+
+(deftest test-no-starting-point
+  (testing
+   (let [labyrinth [[:_ :_ :W :W]
+                    [:W :W :_ :_]
+                    [:W :_ :_ :W]
+                    [:W :W :_ :Exit]]
+         solution  [[:_ :_ :W :W]
+                    [:W :W :_ :_]
+                    [:W :_ :_ :W]
+                    [:W :W :_ :Exit]]]
+    (is (= solution (solve labyrinth))))))
+
+(deftest test-no-solution 
+  (testing
+   (let [labyrinth [[:W :_ :W :W]
+                    [:W 1 :_ :_]
+                    [:W :_ :_ :W]
+                    [:W :W :W :Exit]]
+         solution  ["unable to exit"]]
+    (is (= solution (solve labyrinth))))))
+
+(deftest test-optimal-solution 
+  (testing
+   (let [labyrinth [[:_ :_ :_ :_ :_]
+                    [:_ :_ :_ :_ :_]
+                    [:_ :_ 1 :_ :Exit]
+                    [:_ :_ :_ :_ :_]]
+         solution [[:_ :_ :_ :_ :_]
+                   [:_ :_ :_ :_ :_]
+                   [:_ :_ 1 1 1]
+                   [:_ :_ :_ :_ :_]]]
+     (is (= solution (solve labyrinth))))))
